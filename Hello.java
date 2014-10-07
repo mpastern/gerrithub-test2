5b3c41a3548f0602a413375d049140c9cc36ae94
Michael Pasternak <michael.pasternak@kenshoo.com>
2014-10-07 14:32:58 +0300

    2

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world from Luca !");
+                System.out.println("Hello world from 1 !");
+                System.out.println("Hello world from 2 !");
+                System.out.println("Hello world from 3 !");
 	}
 }
