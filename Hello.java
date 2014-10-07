4e23cf7a8ec619d18f1c5d891e86e6201347e3e1
Michael Pasternak <michael.pasternak@kenshoo.com>
2014-10-07 14:45:19 +0300

    3
    Change-Id: I24ddca52ee12a331d3f2cbc9956c67e170ef74a0

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world from Luca !");
+                System.out.println("Hello world from Luca 1");
+                System.out.println("Hello world from Luca 2");
 	}
 }
